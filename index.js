1b62af6a28b115412ca2da7d3652034938780e43
TheMagicNacho <justinhasanaccount@gmail.com>
2022-01-26 10:03:18 -0500

    new day

--- index.js
+++ index.js
@@ -1,135 +1,91 @@
-// /*
-// Bubble sort is the most basic sorting algorithm in all of Computer Sciencedom. It works by starting at the first element of an array and comparing it to the second element; if the first element is greater than the second element, it swaps the two. It then compares the second to the third, and the third to the fourth, and so on; in this way, the largest values "bubble" to the end of the array. Once it gets to the end of the array, it starts over and repeats the process until the array is sorted numerically.
-// Implement a function that takes an array and sorts it using this technique. Don't use JavaScript's built-in sorting function (Array.prototype.sort).
-// QUERY: What's the time complexity of your algorithm? If you don't already know, try to intuit this without consulting the Googles.
-// Extra credit: Optimization time! During any given pass, if no elements are swapped we can assume the list is sorted and can exit the function early. After this optimization, what is the time complexity of your algorithm?
+// var numbersToWords = {
+//   0: "zero",
+//   1: "one",
+//   2: "two",
+//   3: "three",
+//   4: "four",
+//   5: "five",
+//   6: "six",
+//   7: "seven",
+//   8: "eight",
+//   9: "nine",
+//   10: "ten",
+//   11: "eleven",
+//   12: "twelve",
+//   13: "thirteen",
+//   14: "fourteen",
+//   15: "fifteen",
+//   16: "sixteen",
+//   17: "seventeen",
+//   18: "eighteen",
+//   19: "nineteen",
+//   20: "twenty",
+//   30: "thirty",
+//   40: "forty",
+//   50: "fifty",
+//   60: "sixty",
+//   70: "seventy",
+//   80: "eighty",
+//   90: "ninety"
+// };
+// var numbersToPlace = {
+//   10: "ten",
+//   100: "hundred",
+//   1000: "thousand",
+//   1000000: "million",
+//   1000000000: "billion",
+//   1000000000000: "trillion",
+//   1000000000000000: "quadrillion",
+//   1000000000000000000: "quintillion"
+// };
+// Number.prototype.toEnglish = function() {
+//       //condition & set variables
+//     // const inputString = this.toString();
+//     const inputInt = parseInt(this);
+//     let itterator = 0;
+//     let placeValue = 0;
+//     let place = 0;
+//     let output = '';
 
-// Moar credits: Do you need to consider every element every time you iterate through the array? Make it happen, boss. Again: Has the time complexity of your algorithm changed?
-// Example usage:
-// bubbleSort([2, 1, 3]); // yields [1, 2, 3]
-
-// Feel free to add helper functions if needed.
-
-// */
-
-// //INPUT: Array of int
-// //OUTPUT: Sortted array of int
-// //EDGE:
-// //CONSTRAINTS: Must use bubble sort. Don't use sort
+//   function recurse(){
 
 
-// var bubbleSort = function(array) {
-//   // let output=[];
-//   function a(element, index) {
-//     // console.log(`current element: ${element}`);
-//     let a = element;
-//     let b = array[index+1]
-//       if(a > b){ 
-//         array[index] = b;
-//         array[index+1] = a;
+//     // return my value as english words
+//     //if there's a match in the lookup table
+//     if (numbersToWords[inputInt]) {
+//       output = numbersToWords[inputInt];
+//     } else if (inputInt < 100){
+//       placeValue = Math.floor(inputInt /10)*10 ;
+//       itterator = inputInt % 10;
+//       output = `${numbersToWords[placeValue]} - ${numbersToWords[itterator]}`;
+//     } else {
+//       if (inputInt < 1000){ 
+//         place = 100;
+//       } else {
+//         place = 1000;
+//         while (place * 1000 < inputInt){
+//           place *= 1000;
+//         }
 //       }
+
+//       placeValue = Math.floor(inputInt / place);
+//       itterator = inputInt % place;
+//       output = `${recurse(placeValue)} ${numbersToPlace[place]}`;
+//       let remainder = recurse(itterator);
+//       if (remainder !== 'zero'){output += remainder};
 //     }
-
-//   for (let i = 0; i < array.length; i++){
-//     array.forEach(a);
-//   }  
-
-//   return array;
+//     return output
+//   }
+//   return recurse(this);
 // };
 
+// const toEnglish = Number.prototype.toEnglish;
+
+
+// let testMe = 1000; 
+
+// console.log(testMe.toEnglish());
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
-
-// module.exports = firstNonRepeatedCharacter;
-
-/*
-    function a(element, index) {
-      // console.log(`current element: ${element}`);
-        if(element > tickerBase){ 
-          counterArray[index]=0;
-          counterArray[index+1]++;
-        }
-      }
-*/
-  // function sortHelper(element, index) {
-  //   // console.log(`current element: ${element}`);
-  //   let a = element;
-  //   let b = array[index+1]
-  //     if(a > b){ 
-  //       array[index] = b;
-  //       array[index+1] = a;
-  //     }
-  //   };
-
-let c = 0;
-var bubbleSort = function(array) {
-    //let finLength = (array.length * array.length)
-    // let boolArray = [];
-    for(var i = 0; i < array.length; i++){
-      if (array[i] < array[i+1]) {
-          return array;
-          // boolArray.push(false);
-      }
-    }
-    // if(!boolArray.includes(false)){
-    //   return;
-    // }
-
-    // if (array ===  array.sort()) {
-    //   return array;
-    // }
-    if (c === array.length) {
-        c = 0;
-        bubbleSort(array)
-    }
-    else if (array[c] > array[c+1]) 
-    {
-        let tempVal = array[c];
-        array[c] = array[c+1];
-        array[c+1] = tempVal;
-        c += 1;
-        bubbleSort(array)
-    }
-    else {
-        c += 1;
-        bubbleSort(array)
-    }
-};
-
-var result = bubbleSort([ 1, 2, 43, 100, 100, 21, 21 ]);
-console.log(result);
-
-
-
-function bubbleSortRecursion(arr)
-{
-  const n = array.length;
- function recurse(){
-    // Base case
-    if (n == 1)
-        return;
-  
-    // One pass of bubble
-    // sort. After this pass,
-    // the largest element
-    // is moved (or bubbled)
-    // to end.
-     
-    for (var i = 0; i < n - 1; i++)
-        if (arr[i] > arr[i + 1])
-        {
-         
-            // swap arr[i], arr[i+1]
-            var temp = arr[i];
-            arr[i] = arr[i + 1];
-            arr[i + 1] = temp;
-        }
-  
-    // Largest element is fixed,
-    // recur for remaining array
-    bubbleSort(arr, n - 1);
- }
- recurse();
-
-}
